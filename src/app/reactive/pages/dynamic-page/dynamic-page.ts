e282d63a7bd7582be9a47ef49391e7d4add9ef44
Guillermo Moreno Rivera <gmoreno@afdevelopment.com.mx>
2025-10-21 22:22:01 -0600

    Enhance `DynamicPage` functionality and form handling
    - Add submit event handling for the form.
    - Implement dynamic favorite item addition and removal with validation.
    - Introduce `newFavoriteItem` control for new favorite input management.
    - Update template for improved accessibility and dynamic features.
    - Display error messages and control values for debugging purposes.

--- src/app/reactive/pages/dynamic-page/dynamic-page.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.ts
@@ -1,6 +1,6 @@
 import {Component, inject} from '@angular/core';
 import {JsonPipe} from '@angular/common';
-import {FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {FormUtils} from '../../../utils/form-utils';
 
 @Component({
@@ -17,15 +17,20 @@ export class DynamicPage {
     formUtils = FormUtils;
     myForm:FormGroup = this.fb.group({
         name: ['', [Validators.required, Validators.minLength(3)]],
-        favorites: this.fb.array([
-            ['Lo-Fi', Validators.required],
-            ['Hardcore', Validators.required]
+        favorites: this.fb.array(
+        [
         ],
-        Validators.minLength(3)),
+        Validators.minLength(3)
+        ),
     });
+
+    newFavoriteItem = new FormControl('', Validators.required)
 
     get favorites(){
         return this.myForm.get('favorites') as FormArray;
+    }
+    set favorites(value: FormArray){
+        this.myForm.setControl('favorites', value);
     }
 
     onSubmit(){
@@ -34,6 +39,18 @@ export class DynamicPage {
             return;
         }
         console.log(this.myForm.value);
+        this.favorites = this.fb.array([]);
         this.myForm.reset();
+    }
+
+    onAddFavorite(){
+        if(this.newFavoriteItem.invalid) return;
+        const newFavorite = this.newFavoriteItem.value;
+        this.favorites.push(this.fb.control(newFavorite, Validators.required));
+        this.newFavoriteItem.reset();
+    }
+
+    onRemoveFavorite(index:number){
+        this.favorites.removeAt(index);
     }
 }
